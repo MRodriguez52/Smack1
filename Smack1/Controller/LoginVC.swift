0c698c9ee8787d971505e936859d1a87cd19d847
Miguel Rodriguez <miguelrodriguez@Miguels-MacBook-Pro-2.local>
2018-08-05 11:31:59 -0500

    Lesson 4.

---
+++ Smack1/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack1
+//
+//  Created by Miguel Rodriguez on 8/5/18.
+//  Copyright © 2018 Miguel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack1/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack1
+//
+//  Created by Miguel Rodriguez on 8/3/18.
+//  Copyright © 2018 Miguel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+// Segue
+
+let TO_LOGING =  "toLogin"
